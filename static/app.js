b68895fe16502650aadc0f55d74535bd2a6488df
marco mariscal <marco.a.mariscal@gmail.com>
2020-02-23 08:53:55 -0500

    refactoring to be object oriented frontend

--- static/app.js
+++ static/app.js
@@ -1,27 +1,31 @@
-$(async function() {
-  let score = 0;
-  let guessedWords = [];
-  let gameOver = false;
-  $playAgainBtn = $("#play-again");
-  $playAgainBtn.hide();
-  $playAgainBtn.on("click", restartGame);
+class BoggleGame {
+  constructor(gameId, seconds = 60) {
+    this.gameId = "#" + gameId;
+    this.timer(seconds);
+    this.seconds = seconds;
+    this.score = 0;
+    this.guessedWords = [];
+    this.gameOver = false;
 
-  timer(60);
-  $("#guess-form").on("submit", checkWordOnSubmit);
+    $("#guess-form").on("submit", this.handleWordCheck.bind(this));
+  }
 
-  async function checkWordOnSubmit(e) {
-    if (gameOver) return;
+  // send the word to the server to check if it's a valid word
+  async handleWordCheck(e) {
     e.preventDefault();
+    if (this.gameOver) return;
 
     const guess = $("#guess-input").val();
+
+    // multiple checks for a valid input
     if (!guess) return;
     if (guess.length < 2) return; // word should be longer than one character
-    if (guessedWords.includes(guess)) {
+    if (this.guessedWords.includes(guess)) {
       alert("already guessed that word!");
       return;
     }
 
-    guessedWords.push(guess);
+    this.guessedWords.push(guess);
 
     const response = await axios.get(`/guess`, {
       params: { guess: guess }
@@ -33,73 +37,57 @@ $(async function() {
 
     const result = response.data.result;
 
-    displayCheckWordResult(result);
-    updateScore(result, guess);
-    displayScore();
+    this.updateScore(result, guess);
+    this.displayScore(result);
 
-    $(this).trigger("reset");
+    $("#guess-form").trigger("reset");
 
     return result;
   }
 
-  function displayCheckWordResult(result) {
-    $guessResult = $("#guess-word-result");
-    $guessResult.empty();
-    $guessResult.append($("<p>").text(`${result}`));
-  }
-
-  function updateScore(result, guess) {
+  updateScore(result, guess) {
     if (!result) return;
-
-    if (result === "ok") {
-      score += guess.length;
-    } else {
-      return;
-    }
+    this.score = result === "ok" ? this.score + guess.length : this.score;
   }
 
-  function displayScore() {
-    $("#score-display").html(`<p>${score}</p>`);
+  displayScore(result) {
+    $("#guess-word-result").html(`<p>${result}</p>`);
+    $("#score-display").html(`<p>${this.score}</p>`);
   }
 
-  function timer(seconds) {
+  timer(seconds) {
     let remainTime = Date.now() + seconds * 1000;
     let intervalTimer = setInterval(() => {
       let timeLeft = Math.round((remainTime - Date.now()) / 1000);
       if (timeLeft < 0) {
         clearInterval(intervalTimer);
-        endGame();
+        this.endGameDisplay();
+        this.gameOver = true;
         return;
       }
-      $("#timer").html(`<p>${timeLeft}</p>`);
+      $("#timer").html(`<p>Time left: ${timeLeft}</p>`);
     });
   }
 
-  async function endGame() {
-    gameOver = true;
-    let { highscore, plays } = await postScore();
+  async endGameDisplay() {
+    const { highscore, plays } = await this.postScore();
 
-    $("#guess-form").empty();
-    $("#guess-form").html(`
-        <h1>Your score was ${score}!</h1>
-        <h3>Your high score is ${highscore} and you played ${plays} ${plays === 1 ? "time" : "times"}!</h3> 
+    $("#final-results").html(`
+        <p>Your score was ${this.score}!</p>
+        <p>Your high score is ${highscore} and you played ${plays} ${
+      plays === 1 ? "time" : "times"
+    }!</p> 
         `);
-    $playAgainBtn.show();
   }
 
-  async function postScore() {
-    const response = await axios.post("/score", { score: score });
+  async postScore() {
+    const response = await axios.post("/score", { score: this.score });
 
     if (!response) {
       throw new Error("something went wrong with storing your score");
     }
 
-    let { highscore, plays } = response.data;
+    const { highscore, plays } = response.data;
     return { highscore, plays };
   }
-
-  function restartGame() {
-    score = 0;
-    guessedWords = [];
-  }
-});
+}

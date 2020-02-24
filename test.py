f7d18d8405080d29f293d5020905fd91b2fbc562
marco mariscal <marco.a.mariscal@gmail.com>
2020-02-24 09:29:05 -0500

    test update

--- test.py
+++ test.py
@@ -12,38 +12,30 @@ class FlaskTests(TestCase):
         app.config['TESTING'] = True
 
     def test_home_page(self):
-        """Ensure the session is made with appropriate variables and html is displayed."""
-        with self.client as client:
-            response = client.get('/')
+        """Ensure the session is made with appropriate variables."""
+        with self.client:
+            response = self.client.get('/')
             self.assertIn("board", session)
-            self.assertIn("highscore", session)
-            self.assertIn("plays", session)
-            self.assertIn(b"<div><table id='board'>", response.data)
-            self.assertIn(b"<p>Time Left:", response.data)
+            self.assertIsNone(session.get("highscore"))
+            self.assertIsNone(session.get("plays"))
 
     def test_valid_word(self):
         """Check if the word is valid."""
-        with self.client as client:
+        with self.client as client: 
             with client.session_transaction() as sess:
-                sess['board'] = [
-                    ['n', 'i', 'c', 'e', 'e'],
-                    ['n', 'i', 'c', 'e', 'e'],
-                    ['n', 'i', 'c', 'e', 'e'],
-                    ['n', 'i', 'c', 'e', 'e'],
-                    ['n', 'i', 'c', 'e', 'e'], 
-                    ['n', 'i', 'c', 'e', 'e']]
+                sess["board"] = [['m', 'i', 'c', 'e', 'e'],['n', 'i', 'c', 'e', 'e'],['n', 'i', 'c', 'e', 'e'],['n', 'i', 'c', 'e', 'e'],['n', 'i', 'c', 'e', 'e']]
 
-                response = client.get('/guess?guess=nice')
-                self.assertEqual(response.json['result'], 'ok')
+        response = self.client.get('/guess?guess=mi')
+        self.assertEqual(response.json['result'], 'ok')
 
     def test_invalid_word(self):
         self.client.get('/')
-        response = client.get('/guess?guess=not')
+        response = self.client.get('/guess?guess=not')
         self.assertEqual(response.json['result'], 'not-on-board')
 
     def test_non_english_word(self):
         self.client.get('/')
-        response = client.get('/guess?guess=sadfj;lkfja')
+        response = self.client.get('/guess?guess=sadfj;lkfja')
         self.assertEqual(response.json['result'], 'not-word')
         
 
